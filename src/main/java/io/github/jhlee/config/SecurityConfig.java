a9ac51010e1ef465cf763d1256cca991551db9c0
jhlee <iklesd@gmail.com>
2025-07-25 16:23:03 +0900

    chore config 추가

--- src/main/java/io/github/jhlee/config/SecurityConfig.java
+++ src/main/java/io/github/jhlee/config/SecurityConfig.java
@@ -3,35 +3,60 @@ package io.github.jhlee.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    /**
+     *  PasswordEncoder
+     * */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/css/**", "/js/**", "/vendor/**", "/img/**").permitAll()
-                .anyRequest().authenticated()
-        ).formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .permitAll()
-        ).logout(logout-> logout
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-        );
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 설정 (필요시 활성화/비활성화)
+                .authorizeHttpRequests(auth -> auth // 권한 설정
+                        .requestMatchers("/login", "/api/**",
+                                "/swagger-ui.html", "/swagger-ui/**", "/api-docs/**", "/webjars/**",
+                                "/css/**", "/js/**", "/vendor/**", "/img/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
 
-        return http.build();
+                // 로그인
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/index", true)
+                        .failureUrl("/login?error")
+                        .permitAll()
+                )
+
+                // 로그아웃
+                .logout(logout-> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return  http.build();
     }
 
-    /*
-    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    /*public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
                 );
             return http.build();
     }*/
